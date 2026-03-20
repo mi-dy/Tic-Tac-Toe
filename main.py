b02c1ac5b7dca7f6bcd1a920914a54aa5307b43d
mi-dy <mindaugasdy@gmail.com>
2026-03-20 17:09:53 +0200

    Implemented basic enemy program based on random lib

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from tkinter import *
 from tkinter import ttk
 
@@ -19,6 +21,16 @@ def main():
 
             print("Victory royal!")
             return
+
+        enemy_turn(board)
+
+        if check_victory(board):
+            for row in board:
+                print(f"|{row[0]}|{row[1]}|{row[2]}|")
+
+            print("Looser!")
+            return
+
 
 
 def check_victory(board):
@@ -52,6 +64,15 @@ def check_victory(board):
 
     return False
 
+def enemy_turn(board):
+
+    while True:
+        x = random.randint(0, 2)
+        y = random.randint(0, 2)
+
+        if board[x][y] == "":
+            board[x][y] = "0"
+            return board
 
 if __name__ == "__main__":
     main()
